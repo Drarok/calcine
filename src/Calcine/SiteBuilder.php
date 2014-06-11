1602ee994df04b5de0bac413db2309f51ef178a3
Mat Gadd <mat@zerifas.com>
2014-06-11 14:03:29 +0100

    Add individual tag pages and tags index.

--- src/Calcine/SiteBuilder.php
+++ src/Calcine/SiteBuilder.php
@@ -2,6 +2,8 @@
 
 namespace Calcine;
 
+use Calcine\Post\Tag;
+
 use ParsedownExtra;
 use Twig_Environment;
 use Twig_Loader_Filesystem;
@@ -37,6 +39,34 @@ class SiteBuilder
     protected $webPath;
 
     /**
+     * Twig template renderer.
+     *
+     * @var Twig_Environment
+     */
+    protected $twig;
+
+    /**
+     * Markdown parser instance.
+     *
+     * @var ParsedownExtra
+     */
+    protected $parsedown;
+
+    /**
+     * Array of posts, keyed on tag then date.
+     *
+     * @var array
+     */
+    protected $tags = array();
+
+    /**
+     * Array of posts, keyed on date.
+     *
+     * @var array
+     */
+    protected $posts = array();
+
+    /**
      * Constructor.
      *
      * @param User   $user          The user object.
@@ -65,20 +95,35 @@ class SiteBuilder
     }
 
     /**
-     * Build the site, returning how many pages were built.
+     * Build the site, returning how many posts were built.
      *
      * @return int
      */
     public function build()
     {
-        $loader = new Twig_Loader_Filesystem($this->templatesPath);
-        $twig = new Twig_Environment($loader);
+        $this->posts = array();
+        $this->tags = array();
 
+        $loader = new Twig_Loader_Filesystem($this->templatesPath);
+        $this->twig = new Twig_Environment($loader);
+
+        $this->parsedown = new ParsedownExtra();
+
+        $this->buildPosts();
+        $this->buildIndexes();
+
+        return count($this->posts);
+    }
+
+    /**
+     * Build the site, returning how many pages were built.
+     *
+     * @return int
+     */
+    protected function buildPosts()
+    {
         $dir = new \DirectoryIterator($this->postsPath);
 
-        $parsedown = new ParsedownExtra();
-
-        $pageCount = 0;
         foreach ($dir as $fileinfo) {
             if ($dir->isDot()) {
                 continue;
@@ -88,17 +133,68 @@ class SiteBuilder
                 continue;
             }
 
-            ++$pageCount;
-
+            // Parse the post file.
             $post = new Post($fileinfo->getPathname());
 
-            $template = $twig->render('post.html.twig', array(
+            // Write the post to its own file.
+            $template = $this->twig->render('post.html.twig', array(
+                'user' => $this->user,
                 'post' => $post,
-                'body' => $parsedown->text($post->getBody()),
+                'body' => $this->parsedown->text($post->getBody()),
             ));
             file_put_contents(Path::join($this->webPath, $post->getSlug() . '.html'), $template);
+
+            // Store the post in the posts and tags arrays for buildIndexes.
+            $this->posts[$post->getDate()] = $post;
+
+            foreach ($post->getTags() as $tag) {
+                if (! array_key_exists($tag->getName(), $this->tags)) {
+                    $this->tags[$tag->getName()] = array();
+                }
+                $this->tags[$tag->getName()][$post->getDate()] = $post;
+            }
+        }
+    }
+
+    /**
+     * Build the index pages (/index.html, /tags/index.html, /tags/<tag>.html, etc).
+     *
+     * @return void
+     */
+    protected function buildIndexes()
+    {
+        $tagsRoot = Path::join($this->webPath, 'tags');
+
+        if (! is_dir($tagsRoot) && ! mkdir($tagsRoot)) {
+            throw new \Exception('Cannot create \'' . $tagsRoot . '\'.');
         }
 
-        return $pageCount;
+        // Sort by name.
+        ksort($this->tags, SORT_NATURAL);
+
+        // Convert the array of name => posts to real Tag objects.
+        $tags = array();
+        foreach ($this->tags as $name => $posts) {
+            // Sort by date, descending.
+            krsort($posts, SORT_NATURAL);
+            $tag = new Tag($name, $posts);
+            $tags[$name] = $tag;
+        }
+
+        // Build the tags index.
+        $template = $this->twig->render('tags.html.twig', array(
+            'user' => $this->user,
+            'tags' => $tags,
+        ));
+        file_put_contents(Path::join($tagsRoot, 'index.html'), $template);
+
+        // Now build each individual tag page.
+        foreach ($tags as $tag) {
+            $template = $this->twig->render('tag.html.twig', array(
+                'user'  => $this->user,
+                'tag'   => $tag,
+            ));
+            file_put_contents(Path::join($tagsRoot, $tag->getSlug() . '.html'), $template);
+        }
     }
 }
