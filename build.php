86c6dfd1812e2745d5f4fdc4b0192a6c1df583ea
Mat Gadd <mat@zerifas.com>
2014-09-04 15:10:49 +0100

    Remove PathService.

--- build.php
+++ build.php
@@ -6,7 +6,6 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 use Calcine\Config\Parser;
 use Calcine\Path;
-use Calcine\Path\PathService;
 use Calcine\SiteBuilder;
 use Calcine\Template\TemplateRenderer;
 use Calcine\User;
@@ -21,21 +20,14 @@ $user = new User($config->get('user.name'), $config->get('user.email'));
 $templateRenderer = new TemplateRenderer(
     $user,
     $config->get('paths.templates'),
-    $config->get('paths.posts'),
     $config->get('paths.web')
 );
 $templateRenderer->setTheme($config->get('site.theme'));
 
-$pathService = new PathService(
-    $config->get('paths.posts'),
-    $config->get('paths.templates'),
-    $config->get('paths.web')
-);
-
 $site = new SiteBuilder(
     $user,
     $templateRenderer,
-    $pathService
+    $config->get('paths.posts')
 );
 
 $stats = $site->build();
