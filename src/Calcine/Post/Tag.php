e0532e94b57bee04bc50aa440235339a149e9adf
Mat Gadd <source@catchall.drarok.com>
2025-01-09 21:06:27 +0000

    Make Post and Tag simple readonly data classes

--- src/Calcine/Post/Tag.php
+++ src/Calcine/Post/Tag.php
@@ -4,52 +4,10 @@ namespace Calcine\Post;
 
 use Calcine\Post;
 
-class Tag
+readonly class Tag
 {
-    /**
-     * Tag name.
-     *
-     * @var string
-     */
-    protected $name;
-
-    /**
-     * Posts.
-     *
-     * @var array
-     */
-    protected $posts;
-
-    /**
-     * Constructor.
-     *
-     * @param string $name Tag name.
-     * @param Post[] $posts Posts that belong to this tag.
-     */
-    public function __construct($name, array $posts = array())
+    public function __construct(public string $name, public array $posts = [])
     {
-        $this->name = $name;
-        $this->posts = $posts;
-    }
-
-    /**
-     * Gets the tag name.
-     *
-     * @return string
-     */
-    public function getName()
-    {
-        return $this->name;
-    }
-
-    /**
-     * Gets the posts.
-     *
-     * @return Post[]
-     */
-    public function getPosts()
-    {
-        return $this->posts;
     }
 
     /**
@@ -59,6 +17,6 @@ class Tag
      */
     public function getSlug()
     {
-        return trim(preg_replace('/[^a-z0-9-]+/', '-', strtolower($this->getName())), '-');
+        return trim(preg_replace('/[^a-z0-9-]+/', '-', strtolower($this->name)), '-');
     }
 }
