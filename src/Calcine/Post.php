e44969bc99c721ba43a0a6bbdb9c0058b9ae76f0
Mat Gadd <source@catchall.drarok.com>
2025-01-21 00:31:27 +0000

    Use rendered markdown for shortened body, with tests
    This ensures the HTML is valid, previously links defined at the end of a post would fail to render.

--- src/Calcine/Post.php
+++ src/Calcine/Post.php
@@ -23,26 +23,4 @@ readonly class Post
             throw new \Exception('Invalid data: ' . implode(', ', $errors));
         }
     }
-
-    public function getShortBody(): string
-    {
-        $body = trim($this->body);
-
-        // Manually-specified "jump" marker.
-        $jumpLocation = stripos($body, '<!-- jump -->');
-
-        // Attempt to find the end of a paragraph.
-        if ($jumpLocation === false) {
-            $jumpLocation = strpos($body, "\n\n");
-        }
-
-        if ($jumpLocation) {
-            return substr($body, 0, $jumpLocation);
-        } else {
-            // Fall-back behavior is to return a limited number of words.
-            $words = preg_split('/\\b/', $body);
-            $words = array_slice($words, 0, 150);
-            return implode('', $words);
-        }
-    }
 }
--- src/Calcine/Template/CustomTwigEnvironment.php
+++ src/Calcine/Template/CustomTwigEnvironment.php
@@ -7,13 +7,35 @@ use Twig\Extra\Markdown\DefaultMarkdown;
 use Twig\Extra\Markdown\MarkdownExtension;
 use Twig\Extra\Markdown\MarkdownRuntime;
 use Twig\Loader\LoaderInterface;
+use Twig\Markup;
 use Twig\RuntimeLoader\RuntimeLoaderInterface;
 
 class CustomTwigEnvironment extends TwigEnvironment
 {
+    public static function shortenPostBody(string $body): Markup
+    {
+        // Manually-specified "jump" marker.
+        $jumpLocation = stripos($body, '<!-- jump -->');
+
+        // Attempt to find the end of a paragraph.
+        if ($jumpLocation === false && ($position = strpos($body, '</p>', 1)) !== false) {
+            $jumpLocation = $position + 4;
+        }
+
+        // Fall-back behavior is to return the whole string, since we don't want invalid HTML.
+        if ($jumpLocation === false) {
+            $jumpLocation = strlen($body);
+        }
+
+        return new Markup(substr($body, 0, $jumpLocation), 'UTF-8');
+    }
+
     public function __construct(LoaderInterface $loader, array $options = [])
     {
         parent::__construct($loader, $options);
+
+        $shortenFilter = new \Twig\TwigFilter('shorten_post_body', [static::class, 'shortenPostBody']);
+        $this->addFilter($shortenFilter);
 
         $this->addExtension(new MarkdownExtension());
         $this->addRuntimeLoader(new class implements RuntimeLoaderInterface {
